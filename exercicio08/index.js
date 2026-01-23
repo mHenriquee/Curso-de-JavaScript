8e82856b10320e414eaa86fc9b0ab72b2c30768f
Mario Henrique <mariohenrique.s@hotmail.com>
2026-01-23 17:13:14 -0300

    exercicios

---
+++ exercicio08/index.js
@@ -0,0 +1,9 @@
+var velocidade = 90.5
+
+console.log (`A velocidade do seu carro é ${velocidade}km/h`)
+
+if (velocidade > 80) {
+    console.log (`Você ultrapassou a velocidade permitida!`)
+}
+
+console.log (`Dirija sempre usando cinto de segurança`)
